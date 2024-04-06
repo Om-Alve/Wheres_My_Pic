02cc9571ff78d7cd5563c07d38f61b508c6da76f
Om-Alve <omalve8174@gmail.com>
2024-04-06 22:56:26 +0530

    Improved ui and fixed some bugs

--- app.py
+++ app.py
@@ -2,17 +2,14 @@ import gradio as gr
 import chromadb
 import torch
 import torch.nn as nn
-
 from PIL import Image
 from transformers import CLIPProcessor, CLIPModel
 import numpy as np
 import os
 
 print("Loading model...")
-
 model = CLIPModel.from_pretrained("./CLIP-VIT").to('cuda')
 processor = CLIPProcessor.from_pretrained("./CLIP-VIT")
-
 print("Model loaded!")
 
 client = chromadb.PersistentClient('img_db/')
@@ -23,16 +20,19 @@ def search(query):
         text_emb = model.get_text_features(**processor.tokenizer(query, return_tensors='pt').to('cuda'))
     results = collection.query(
         query_embeddings=text_emb.cpu().squeeze(0).tolist(),
-        n_results=5, # top n results
+        n_results=4  # top n results
     )
-    return Image.open(results['documents'][0][0])
+    # Create a gallery of the top 5 results
+    gallery_images = [Image.open(doc) for doc in results['documents'][0]]
+    return gallery_images
 
-interface = gr.Interface(
+demo = gr.Interface(
     fn=search,
-    inputs=gr.Textbox(lines=1, placeholder="Enter your text query..."),
-    outputs=gr.Image(type="pil"),
-    title="Where's My Picture? : A Local Image Search Engine",
-    description="Search for images in your local folder using text queries.",
+    inputs=gr.Textbox(placeholder="Enter a query"),
+    outputs=gr.Gallery(label="Results",selected_index=0,preview=True),
+    title="Where's My Pic?",
+    description="A local image search engine powered by CLIP!",
+    theme = gr.themes.Default(primary_hue="purple")
 )
 
-interface.launch(debug=True)
+demo.launch()
