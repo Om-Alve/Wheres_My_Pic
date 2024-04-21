4864360680276245dbbf24712816ae947a6fc71d
Om-Alve <omalve8174@gmail.com>
2024-04-21 13:31:47 +0530

    Added functionality to add multiple folders

--- build-db.py
+++ build-db.py
@@ -6,12 +6,12 @@ from transformers import CLIPProcessor, CLIPModel
 import numpy as np
 import os
 import time
+import argparse
 
 print("Loading model...")
-device = 'cpu'
+device = 'cuda'
 model = CLIPModel.from_pretrained("./CLIP-VIT").to(torch.bfloat16).to(device)
 processor = CLIPProcessor.from_pretrained("./CLIP-VIT")
-
 print("Model loaded!")
 
 def extract_features_clip(image):
@@ -27,58 +27,84 @@ def create_db(folder, collection):
     filenames = []
     start_time = time.time()
     for image in os.listdir(folder):
-        filename = os.path.join(folder,image)
+        filename = os.path.join(folder, image)
         img = Image.open(filename)
         img_embeddings.append(extract_features_clip(img))
         filenames.append(filename)
-    
-
     collection.add(
-        embeddings = img_embeddings, # embeddings are used to search through the documents
-        documents=filenames, # we send in the titles as the documents
-        ids=filenames, # must be unique for each doc
+        embeddings=img_embeddings,
+        documents=filenames,
+        ids=filenames,
     )
     end_time = time.time()
-    print("Database updated successfully!")
+    print(f"Database updated for {folder} successfully!")
     print(f"Time taken: {(end_time - start_time) * 1000} milliseconds")
 
-
-def update_db(folder, collection):
+def update_db(folders, collection):
     start_time = time.time()
-    current_files = [os.path.join(folder, image) for image in os.listdir(folder) if os.path.isfile(os.path.join(folder, image))]
+    current_files = []
+    for folder in folders:
+        current_files.extend([os.path.join(folder, image) for image in os.listdir(folder) if os.path.isfile(os.path.join(folder, image))])
+
     existing_files = collection.get()['documents']
     new_files = [file for file in current_files if file not in existing_files]
     deleted_files = [file for file in existing_files if file not in current_files]
+
     if len(new_files) == 0 and len(deleted_files) == 0:
-        print("Nothing to update!")
+        print(f"Nothing to update for {', '.join(folders)}!")
         return
+
     for file in deleted_files:
         idx = existing_files.index(file)
         collection.delete(ids=[file])
 
-    for i,file in enumerate(new_files):
+    for i, file in enumerate(new_files):
         img = Image.open(file)
         img_embeddings = extract_features_clip(img)
-
         collection.upsert(
             embeddings=[img_embeddings],
             documents=[file],
             ids=[file]
         )
+
     end_time = time.time()
     print("Database updated successfully!")
     print(f"Time taken: {(end_time - start_time) * 1000} milliseconds")
-    
 
-if len(client.list_collections())  != 0:
-    print("Database exists, Updating the database...")
-    collection = client.get_collection('images')
-    update_db('Image-Folder',collection)
-    print(len(collection.get()['documents']))
-else:
-    collection =  client.create_collection(
-        name="images",
-        metadata={"hnsw:space": "cosine"}
-    )
-    create_db('Image-Folder',collection)
-    print("Created the database!")
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(description='Index image folders')
+    parser.add_argument('--add', nargs='+', help='Create a new database with the specified folders')    
+    parser.add_argument('--update', action='store_true', help='Update the existing database')
+    args = parser.parse_args()
+    if len(client.list_collections()) == 0:
+        # if the database hasn't been created before
+        collection = client.create_collection(
+            name="images", metadata={"hnsw:space": "cosine"}
+        )
+        for folder in args.add:
+            create_db(folder, collection)
+        print("Created the database!")
+        with open('img_db/indexed_folders.txt', 'w') as f:
+            f.write(f"{'\n'.join(args.add)}") 
+        print(f"Added Folders {args.add} successfully!")
+        print(f"Total documents indexed: {len(collection.get()['documents'])}")  
+    elif args.add:
+        # if the db is already created and want to add new folders
+        collection = client.get_collection('images')
+        with open('img_db/indexed_folders.txt', 'r') as f:
+            indexed_folders = f.read().split('\n')
+        folders = set(args.add).difference(set(indexed_folders))
+        for folder in folders:
+            create_db(folder, collection)
+        print(f"Added Folders {args.add} successfully!")
+        print(f"Total documents indexed: {len(collection.get()['documents'])}")  
+        with open('img_db/indexed_folders.txt', 'a') as f:
+            f.write(f"\n{'\n'.join(args.add)}")    
+    else:
+        # if the db is already created and want to update the existing folders
+        collection = client.get_collection('images')
+        with open('img_db/indexed_folders.txt', 'r') as f:
+            indexed_folders = f.read().split('\n')
+        update_db(indexed_folders, collection)
+        print(f"Total documents indexed: {len(collection.get()['documents'])}")  
+
